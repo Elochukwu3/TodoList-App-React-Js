741b507f42ea9df411822b109981f6e5ff1a4fae
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-15 00:20:21 +0100

    savav

--- src/App.js
+++ src/App.js
@@ -1,51 +1,58 @@
-
-import { useState } from 'react';
-import './App.css';
-import TodoList from './Compontents/TodoList';
+import { useState } from "react";
+import "./App.css";
+import TodoList from "./Compontents/TodoList";
 let next = 2;
-const initia =[
-  {id:1,
-  text:"I am good",
-done:false
-}
-
-]
+const initia = [{ id: 1, text: "I am good", done: false }];
 function App() {
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState(initia);
 
   function addTodo(e) {
-    e.preventDefault()
-   setTodoList( [...todoList, {id: next++, text: todo, done:false,}])
-   setTodo('')
-   
-  };
-  
+    e.preventDefault();
+    setTodoList([...todoList, { id: next++, text: todo, done: false }]);
+    setTodo("");
+  }
+
   function handleAddTodo(nextTodo) {
-      setTodoList(todoList.map(t => {
+    setTodoList(
+      todoList.map((t) => {
         if (t.id === nextTodo.id) {
           return nextTodo;
         } else {
           return t;
         }
-      }));
-   
+      })
+    );
   }
   function deleteBtn(p) {
-    setTodoList(todoList.filter((e)=>{
-      const{id} = e;
-      return id !== p;
-    }))
+    setTodoList(
+      todoList.filter((e) => {
+        const { id } = e;
+        return id !== p;
+      })
+    );
   }
   return (
     <div className="App">
-        <h1>Todo Application</h1>
+     <div className="AppInner">
+     <h1>Todo Application</h1>
       <form onSubmit={addTodo}>
-      <input  value={todo} className="toInput" onChange={(e)=>setTodo(e.target.value)} required/>
-        <button type='submit' className='btnOne'>Add Todo</button>
+        <input
+          value={todo}
+          className="toInput"
+          onChange={(e) => setTodo(e.target.value)}
+          required
+        />
+        <button type="submit" className="btnOne">
+          Add Todo
+        </button>
       </form>
-      <TodoList todoItem={todoList} change={deleteBtn} inputChange={handleAddTodo}/>
-    
+      <TodoList
+        todoItem={todoList}
+        change={deleteBtn}
+        inputChange={handleAddTodo}
+      />
+     </div>
     </div>
   );
 }
