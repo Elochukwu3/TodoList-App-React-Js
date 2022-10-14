c4e4fe35b6c336ad53ebe92d1507bf679361c348
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-14 19:45:33 +0100

    save Todos

--- src/Compontents/TodoList.jsx
+++ src/Compontents/TodoList.jsx
@@ -1,37 +1,58 @@
 import { useState } from "react";
 
-
-
-export default function TodoList({todoItem, change}) {
+export default function TodoList({ todoItem, change }) {
   return (
     <>
-        {todoItem.length > 0 ? <ul>
-         {
-               (todoItem.map((eachTodo, i)=>{
-                const {id} = eachTodo;
-                return (<li key={id}>
-               <Task onDelete={change} todos={eachTodo}/>
-                </li>  
-               )
-            }))
-           
-         }
-         
-        </ul>:''}
+      {todoItem.length > 0 ? (
+        <ul>
+          {todoItem.map((eachTodo, i) => {
+            const { id } = eachTodo;
+            return (
+              <li key={id}>
+                <Task onDelete={change} todos={eachTodo} />
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        ""
+      )}
     </>
-  )
+  );
 }
 
-function Task({todos, onDelete}) {
-    const[validating, setValidating] = useState(false);
-    let content;
-   validating ?content=(<><input value={todos.text}/>  <button onClick={() =>{setValidating(true)}} >Edit</button></>):
-   content = (<><input value={todos.text} readOnly/> <button onClick={() =>{setValidating(false)}} >save</button></>);
-    return(
+function Task({ todos, onDelete }) {
+  const [validating, setValidating] = useState(false);
+  let content;
+  validating
+    ? (content = (
         <>
-        <p>{content}</p>
-        <button onClick={()=>onDelete(todos.id)}>Delete</button>
+          <input value={todos.text} />{" "}
+          <button
+            onClick={() => {
+              setValidating(false);
+            }}
+          >
+            Save
+          </button>
         </>
-    )
-    
+      ))
+    : (content = (
+        <>
+          <input value={todos.text} readOnly />{" "}
+          <button
+            onClick={() => {
+              setValidating(true);
+            }}
+          >
+            Edit
+          </button>
+        </>
+      ));
+  return (
+    <>
+      <p>{content}</p>
+      <button onClick={() => onDelete(todos.id)}>Delete</button>
+    </>
+  );
 }
